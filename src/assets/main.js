df77bce0f5758712e03b32672ea53d5daf65cfcf
Parimal Chandwaskar <parimalchandwaskar@parimals-mbp.attlocal.net>
2017-07-02 18:20:37 -0400

    Jquery badge project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,41 @@
 $(function() {
-
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/sergiocruz.json',
+  	dataType: 'jsonp',
+  	success: function(response){
+  		console.log(response.courses.completed);
+  		debugger
+  		addDynamicData(response.courses.completed);
+  	}
+  });
+
+  addDynamicData = function(course){
+ 	var $badges = $("#badges");
+  	for(var i=0; i<course.length;i++){
+  		
+  		var $course = $("<div />", {
+  			'class':'course'
+  		}).appendTo($badges);
+
+  		$('<h3 />', {
+  			text: course[i].title
+  		}).appendTo($course);
+
+  		$('<img />',{
+  			src: course[i].badge
+  		}).appendTo($course);
+
+  		$('<a />', {
+  			'class': 'btn btn-primary',
+  			'target': '_blank',
+  			 href: course[i].url,
+  			 text: 'See Course'
+  		}).appendTo($course);
+  	}
+  };
 
 });
+
+
+
